d11e14c1a15d4e7f00ddd8f87675d3beda02814f
That Random <andrewwoan@gmail.com>
2022-05-15 17:09:30 -0400

    main

--- main.js
+++ main.js
@@ -5,6 +5,24 @@ const experience = new Experience(document.querySelector("canvas.experience"));
 const menuBtn = document.querySelector(".menu-button");
 const nav = document.querySelector(".nav-menu");
 
+const content = document.querySelector(".content");
+
+const close = document.querySelector(".close");
+
+const listItems = document.querySelectorAll("hide");
+
 menuBtn.addEventListener("click", () => {
     nav.classList.toggle("hidden");
+    content.classList.add("hidden");
+});
+
+listItems.forEach((item) => {
+    item.addEventListener("click", () => {
+        nav.classList.toggle("hidden");
+    });
+});
+
+close.addEventListener("click", () => {
+    nav.classList.toggle("hidden");
+    content.classList.add("hidden");
 });
