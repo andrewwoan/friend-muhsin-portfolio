a77c8bb882f11e0851bb6945bbe68bf65ca00c26
That Random <andrewwoan@gmail.com>
2022-05-21 06:37:17 -0400

    ok

--- Experience/Raycaster.js
+++ Experience/Raycaster.js
@@ -320,7 +320,7 @@ export default class Raycaster extends EventEmitter {
             <div class="title">Credits
             </div>
             <li><a href="https://www.crossyroad.com/" target="_blank" rel="noopener noreferrer">[LINK] Theme inspired by Crossy Road </a></li>
-            <li><a href="https://bruno-simon.com/" target="_blank" rel="noopener noreferrer">[LINK] Interactions inspired by Bruno Simon's Portfolio</a></li>
+            <li><a href="https://bruno-simon.com/" target="_blank" rel="noopener noreferrer">[LINK] Idea inspired by Bruno Simon's Portfolio</a></li>
             <li><a href="https://threejs-journey.xyz/" target="_blank" rel="noopener noreferrer">[LINK] Check out Bruno's course to learn how to make awesome websites</a></li>
             <div class="title">Technology Used
             </div>
@@ -572,8 +572,7 @@ export default class Raycaster extends EventEmitter {
             <div class="title">Credits
             </div>
             <li><a href="https://www.crossyroad.com/" target="_blank" rel="noopener noreferrer">[LINK] Theme inspired by Crossy Road </a></li>
-            <li><a href="https://bruno-simon.com/" target="_blank" rel="noopener noreferrer">[LINK] Interactions inspired by Bruno Simon's Portfolio</a></li>
-            <li><a href="https://threejs-journey.xyz/" target="_blank" rel="noopener noreferrer">[LINK] Check out Bruno's course to learn how to make awesome websites</a></li>
+            <li><a href="https://bruno-simon.com/" target="_blank" rel="noopener noreferrer">[LINK] Idea inspired by Bruno Simon's Portfolio</a></li>
             <div class="title">Technology Used
             </div>
             <li><a href="https://www.figma.com/" target="_blank" rel="noopener noreferrer">[LINK] Figma - Design of portfolio and used to create SVGs </a></li>
